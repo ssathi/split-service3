6ce6f0212f2e8ed73eab1dcd154f48f5e8d7458f
sathi2 <sathi@wired2perform.com>
2019-02-09 08:45:38 +0530

    sfs

---
+++ src/main/java/com/example/splitservice3/SplitService3Application.java
@@ -0,0 +1,14 @@
+package com.example.splitservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitService3Application.class, args);
+	}
+
+}
+
